b864341a621150d22669be3747d742b40eb8167b
Enzo Mateus Gonçalves <enzomateusgonc@gmail.com>
2022-05-28 20:48:56 -0300

    função concluída com sucesso

--- public/signIn.js
+++ public/signIn.js
@@ -1,19 +1,24 @@
 const password = document.querySelector('#password');
 const verify_password = document.querySelector('#verify_password');
+const age_input = document.querySelector('#age');
 const message_error = document.querySelector('.message_error');
 const btn_create = document.querySelector("#create_account")
 
 verify_password.addEventListener('keyup', () => {
-    if(verify_password.value !== password.value) {
-        message_error.textContent = "A senha não é a mesma";
-        btn_create.disabled = true;
-    } else {
-        message_error.textContent = "";
-        btn_create.disabled = false;
-    }
+    verifyPassword()
 })
 
 btn_create.addEventListener('click', () => {
+    verifyPassword()
+    isAge()
+})
+
+age_input.addEventListener('input', (e) => {
+    btn_create.disabled = false;
+    console.log(e)
+})
+
+function verifyPassword() {
     if(verify_password.value !== password.value) {
         message_error.textContent = "A senha não é a mesma";
         btn_create.disabled = true;
@@ -21,4 +26,33 @@ btn_create.addEventListener('click', () => {
         message_error.textContent = "";
         btn_create.disabled = false;
     }
-})
+}
+
+function realAge(fullAge) {
+    let age = 0;
+    const year = new Date().getFullYear();            
+    const month = new Date().getMonth() + 1;            
+    const day = new Date().getDate();
+
+    const ageYear = fullAge.slice(0, 4);
+    const ageMonth = fullAge.slice(5, 7);
+    const ageDay = fullAge.slice(8, 11)
+
+
+    if(ageMonth < month) {
+        age = year - ageYear
+    }  else if(ageMonth == month && ageDay <= day) {
+        age = year - ageYear
+    } else {
+        age = year - ageYear - 1
+    }
+
+    return age;
+}
+
+function isAge() {
+    if(realAge(age_input.value) >= 120) {
+        confirm("Idade não aceita");
+        btn_create.disabled = true;
+    }
+}

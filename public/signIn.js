47ab9de7e541de4f1aaf77b311026eff33752c6f
Enzo Mateus Gonçalves <enzomateusgonc@gmail.com>
2022-05-28 20:25:40 -0300

    layout consertado

--- public/signIn.js
+++ public/signIn.js
@@ -4,7 +4,11 @@ const message_error = document.querySelector('.message_error');
 const btn_create = document.querySelector("#create_account")
 
 verify_password.addEventListener('keyup', () => {
-    if(verify_password.value == password.value) {
+    if(verify_password.value !== password.value) {
+        message_error.textContent = "A senha não é a mesma";
+        btn_create.disabled = true;
+    } else {
+        message_error.textContent = "";
         btn_create.disabled = false;
     }
 })
@@ -14,6 +18,7 @@ btn_create.addEventListener('click', () => {
         message_error.textContent = "A senha não é a mesma";
         btn_create.disabled = true;
     } else {
+        message_error.textContent = "";
         btn_create.disabled = false;
     }
 })
